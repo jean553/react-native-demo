b9ee258cbe1ed30e1301f41dea0323ca34c304cc
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2019-02-24 13:41:51 +0100

    integrate camera preview

--- ReactNativeDemo/TakePictureScreen.js
+++ ReactNativeDemo/TakePictureScreen.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import {
     View,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity,
+    Text
 } from 'react-native';
 
-import Camera from 'expo';
+import {
+    Camera,
+    Permissions
+} from 'expo';
 
 export default class TakePictureScreen extends React.Component {
 
@@ -15,11 +20,43 @@ export default class TakePictureScreen extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            hasCameraPermission: null,
+            type: Camera.Constants.Type.back,
+        };
     }
 
     render() {
         return (
             <View style={styles.container}>
+                <Camera style={{ flex: 1 }} type={this.state.type}>
+                    <View
+                        style={{
+                            flex: 1,
+                            backgroundColor: 'transparent',
+                            flexDirection: 'row',
+                        }}>
+                        <TouchableOpacity
+                            style={{
+                                flex: 0.1,
+                                alignSelf: 'flex-end',
+                                alignItems: 'center',
+                            }}
+                            onPress={() => {
+                                this.setState({
+                                    type: this.state.type === Camera.Constants.Type.back
+                                        ? Camera.Constants.Type.front
+                                        : Camera.Constants.Type.back,
+                                });
+                            }}>
+                            <Text
+                                style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                                {' '}Flip{' '}
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </Camera>
             </View>
         );
     }
