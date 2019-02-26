63ff10e922cb12f55693f7314ab7227d63b1c6c2
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2019-02-26 21:06:44 +0100

    integrate left side menu

--- ReactNativeDemo/App.js
+++ ReactNativeDemo/App.js
@@ -5,16 +5,24 @@ import {
     createAppContainer
 } from 'react-navigation';
 
+import DrawerNavigator from './DrawerNavigator';
 import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
 import TakePictureScreen from './TakePictureScreen';
 
-const MainNavigator = createStackNavigator({
-    Login: {screen: LoginScreen},
-    Home: {screen: HomeScreen},
-    TakePicture: {screen: TakePictureScreen}
-});
+const MainNavigator = createStackNavigator(
+    {
+        DrawerNavigator: {screen: DrawerNavigator},
+        Login: {screen: LoginScreen},
+        Home: {screen: HomeScreen},
+        TakePicture: {screen: TakePictureScreen}
+    },
+    navigationOptions = {
+        headerMode: 'none'
+    }
+);
+
 
 const App = createAppContainer(MainNavigator);
 
-export default App;
+export default App; 
